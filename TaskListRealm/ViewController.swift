8cf33a9d4f3ec3a51360fa3ae8a9c124bc9f8991
Matvei95 <khlestov.95@mail.ru>
2022-12-21 15:32:05 +0500

    Initial Commit

---
+++ TaskListRealm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskListRealm
+//
+//  Created by Matvei Khlestov on 21.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
