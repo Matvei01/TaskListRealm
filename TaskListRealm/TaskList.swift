e215a9ca5e79bf7d5dc0f8758292d676237e821e
Matvei95 <khlestov.95@mail.ru>
2022-12-21 20:39:38 +0500

    Working with DataManager and StrorageManager. Add Realm.

---
+++ TaskListRealm/TaskList.swift
@@ -0,0 +1,22 @@
+//
+//  TaskList.swift
+//  TaskListRealm
+//
+//  Created by Matvei Khlestov on 21.12.2022.
+//
+
+import Foundation
+import RealmSwift
+
+class TaskList: Object {
+    @Persisted var name = ""
+    @Persisted var date = Date()
+    @Persisted var tasks = List<Task>()
+}
+
+class Task: Object {
+    @Persisted var name = ""
+    @Persisted var note = ""
+    @Persisted var date = Date()
+    @Persisted var isComplete = false
+}
